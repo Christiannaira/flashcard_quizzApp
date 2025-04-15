ae15da02a815d1259e1bac7dd99a4b7aa3ee8a4f
Christian <Christianmnaira@gmail.com>
2025-04-15 11:27:05 +0800

    feat: structured the start menu of the quiz app

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("============================= Welcome to FlashCard Quiz App =============================");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type \"start\" to start your quiz :) ");
+        String userSelection = scanner.nextLine();
+
+        if (userSelection.trim().equalsIgnoreCase("start")){
+            System.out.println("begin");
+        }
 
 
 
